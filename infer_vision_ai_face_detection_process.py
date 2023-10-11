948e75c9622bf8a9bfab63c020fbad3024f65c98
allankouidri <allan.kouidri@gmail.com>
2023-10-11 17:10:04 +0200

    Edit algo type: object detection to keypoints

--- infer_vision_ai_face_detection_process.py
+++ infer_vision_ai_face_detection_process.py
@@ -1,6 +1,5 @@
 import copy
 from ikomia import core, dataprocess
-
 from google.cloud import vision
 import os
 import io
@@ -40,10 +39,10 @@ class InferVisionAiFaceDetectionParam(core.CWorkflowTaskParam):
 # - Class which implements the algorithm
 # - Inherits PyCore.CWorkflowTask or derived from Ikomia API
 # --------------------
-class InferVisionAiFaceDetection(dataprocess.CObjectDetectionTask):
+class InferVisionAiFaceDetection(dataprocess.CKeypointDetectionTask):
 
     def __init__(self, name, param):
-        dataprocess.CObjectDetectionTask.__init__(self, name)
+        dataprocess.CKeypointDetectionTask.__init__(self, name)
         # Add input/output of the algorithm here
         # Example :  self.add_input(dataprocess.CImageIO())
         #           self.add_output(dataprocess.CImageIO())
@@ -55,8 +54,64 @@ class InferVisionAiFaceDetection(dataprocess.CObjectDetectionTask):
             self.set_param_object(copy.deepcopy(param))
 
         self.client = None
-        self.names = ["face"]
-        self.set_names(self.names)
+        self.classes = ["face"]
+        self.landmark_dict = {
+            "LEFT_EYE": 0, "RIGHT_EYE": 1, "LEFT_OF_LEFT_EYEBROW": 2, 
+            "RIGHT_OF_LEFT_EYEBROW": 3, "LEFT_OF_RIGHT_EYEBROW": 4, 
+            "RIGHT_OF_RIGHT_EYEBROW": 5, "MIDPOINT_BETWEEN_EYES": 6, 
+            "NOSE_TIP": 7, "UPPER_LIP": 8, "LOWER_LIP": 9, 
+            "MOUTH_LEFT": 10, "MOUTH_RIGHT": 11, "MOUTH_CENTER": 12,
+            "NOSE_BOTTOM_RIGHT": 13, "NOSE_BOTTOM_LEFT": 14, 
+            "NOSE_BOTTOM_CENTER": 15, "LEFT_EYE_TOP_BOUNDARY": 16,
+            "LEFT_EYE_RIGHT_CORNER": 17, "LEFT_EYE_BOTTOM_BOUNDARY": 18,
+            "LEFT_EYE_LEFT_CORNER": 19, "RIGHT_EYE_TOP_BOUNDARY": 20,
+            "RIGHT_EYE_RIGHT_CORNER": 21, "RIGHT_EYE_BOTTOM_BOUNDARY": 22,
+            "RIGHT_EYE_LEFT_CORNER": 23, "LEFT_EYEBROW_UPPER_MIDPOINT": 24,
+            "RIGHT_EYEBROW_UPPER_MIDPOINT": 25, "LEFT_EAR_TRAGION": 26,
+            "RIGHT_EAR_TRAGION": 27, "FOREHEAD_GLABELLA": 28, 
+            "CHIN_GNATHION": 29, "CHIN_LEFT_GONION": 30, 
+            "CHIN_RIGHT_GONION": 31, "LEFT_CHEEK_CENTER": 32, 
+            "RIGHT_CHEEK_CENTER": 33
+        }
+
+        self.skeleton = [
+            # Eyes
+            [self.landmark_dict["LEFT_EYE"], self.landmark_dict["RIGHT_EYE"]],
+            # Eyebrows
+            [self.landmark_dict["LEFT_OF_LEFT_EYEBROW"], self.landmark_dict["RIGHT_OF_LEFT_EYEBROW"]],
+            [self.landmark_dict["LEFT_OF_RIGHT_EYEBROW"], self.landmark_dict["RIGHT_OF_RIGHT_EYEBROW"]],
+            # Nose
+            [self.landmark_dict["NOSE_TIP"], self.landmark_dict["MIDPOINT_BETWEEN_EYES"]],
+            [self.landmark_dict["NOSE_BOTTOM_LEFT"], self.landmark_dict["NOSE_BOTTOM_CENTER"]],
+            [self.landmark_dict["NOSE_BOTTOM_RIGHT"], self.landmark_dict["NOSE_BOTTOM_CENTER"]],
+            [self.landmark_dict["NOSE_TIP"], self.landmark_dict["NOSE_BOTTOM_CENTER"]],
+            # Mouth
+            [self.landmark_dict["UPPER_LIP"], self.landmark_dict["MOUTH_LEFT"]],
+            [self.landmark_dict["LOWER_LIP"], self.landmark_dict["MOUTH_RIGHT"]],
+            [self.landmark_dict["MOUTH_RIGHT"], self.landmark_dict["UPPER_LIP"]],
+            [self.landmark_dict["MOUTH_LEFT"], self.landmark_dict["LOWER_LIP"]],
+            # Face contour
+            [self.landmark_dict["LEFT_EAR_TRAGION"], self.landmark_dict["LEFT_OF_LEFT_EYEBROW"]],
+            [self.landmark_dict["RIGHT_EAR_TRAGION"], self.landmark_dict["RIGHT_OF_RIGHT_EYEBROW"]],
+            [self.landmark_dict["LEFT_EAR_TRAGION"], self.landmark_dict["CHIN_LEFT_GONION"]],
+            [self.landmark_dict["RIGHT_EAR_TRAGION"], self.landmark_dict["CHIN_RIGHT_GONION"]],
+            [self.landmark_dict["CHIN_LEFT_GONION"], self.landmark_dict["CHIN_GNATHION"]],
+            [self.landmark_dict["CHIN_RIGHT_GONION"], self.landmark_dict["CHIN_GNATHION"]]
+        ]
+
+        self.palette = [
+            # Eyes
+            [255, 102, 102],
+            # Eyebrows
+            [255, 178, 102], [255, 178, 102],
+            # Nose
+            [153, 255, 153], [230, 230, 0], [230, 230, 0], [230, 230, 0],
+            # Mouth
+            [153, 204, 255], [153, 204, 255], [153, 204, 255], [153, 204, 255], 
+            # Face contour
+            [255, 255, 255],[255, 255, 255],[255, 255, 255],[255, 255, 255], [255, 255, 255], [255, 255, 255]
+        ]
+
 
     def get_progress_steps(self):
         # Function returning the number of progress steps for this algorithm
@@ -72,6 +127,17 @@ class InferVisionAiFaceDetection(dataprocess.CObjectDetectionTask):
 
         # Get parameters :
         param = self.get_param_object()
+
+        # Set Keypoints links
+        keypoint_links = []
+        for (start_pt_idx, end_pt_idx), color in zip(self.skeleton, self.palette):
+            link = dataprocess.CKeypointLink()
+            link.start_point_index = start_pt_idx
+            link.end_point_index = end_pt_idx
+            link.color = color
+            keypoint_links.append(link)
+        self.set_keypoint_links(keypoint_links)
+        self.set_object_names(self.classes)
 
         if self.client is None:
             if param.google_application_credentials:
@@ -91,17 +157,40 @@ class InferVisionAiFaceDetection(dataprocess.CObjectDetectionTask):
         # Inference
         response = self.client.face_detection(image=image)
         faces = response.face_annotations
-
-        # Process output
+        # # Process output
         for i, face in enumerate(faces):
-            # Get coordinates
-            vertices = [(vertex.x,vertex.y) for vertex in face.bounding_poly.vertices]
-            x1 = vertices[0][0]
-            y1 = vertices[0][1]
-            w = vertices[1][0] - x1
-            h = vertices[2][1] - y1
-            # Display
-            self.add_object(i+1, 0, face.detection_confidence, float(x1), float(y1), w, h)
+            if face.detection_confidence < param.conf_thres: # skip detections with lower score
+                continue
+            print(param.conf_thres)
+            # Get box coordinates
+            vertices = [(vertex.x,vertex.y) for vertex in face.fd_bounding_poly.vertices]
+            x_box = vertices[0][0]
+            y_box = vertices[0][1]
+            w = vertices[1][0] - x_box
+            h = vertices[2][1] - y_box
+
+            # Get points coordinates
+            landmarks_objects = face.landmarks
+            kpts_data  = [(landmark.position.x, landmark.position.y) for landmark in landmarks_objects]
+
+            # Set Keypoints links
+            keypts = []
+            kept_kp_id = []
+            for link in self.get_keypoint_links():
+                kp1, kp2 = kpts_data[link.start_point_index], kpts_data[link.end_point_index]
+                x1, y1 = kp1
+                x2, y2 = kp2
+                if link.start_point_index not in kept_kp_id:
+                    kept_kp_id.append(link.start_point_index)
+                    keypts.append(
+                        (link.start_point_index, dataprocess.CPointF(float(x1), float(y1))))
+                if link.end_point_index not in kept_kp_id:
+                    kept_kp_id.append(link.end_point_index)
+                    keypts.append(
+                        (link.end_point_index, dataprocess.CPointF(float(x2), float(y2))))
+
+            # # Display
+            self.add_object(i, 0, face.detection_confidence, float(x_box), float(y_box), w, h, keypts)
 
         # Step progress bar (Ikomia Studio):
         self.emit_step_progress()
